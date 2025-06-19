8bebe2cc18853047698484d8de812253638d1e20
Azmi1180 <muhammadazmi4456@gmail.com>
2025-06-19 16:55:55 +0700

    Welcome Pages and Gender View

--- testingvision/View/GenderView.swift
+++ testingvision/View/GenderView.swift
@@ -8,60 +8,76 @@
 import SwiftUI
 
 struct GenderView: View {
-    @State private var isShowingResult = false    
-    
+    @State private var isShowingResult = false
+
     var body: some View {
-        VStack(alignment: .center, spacing: 180) {
+        VStack(spacing: 0) {
+            // Close button aligned to top trailing
+            Dismiss()
+
             Spacer()
-            
-            VStack(alignment: .center, spacing: 36) {
+
+            VStack(alignment: .leading, spacing: 12) {
                 Text("What’s your gender?")
-                    .font(Font.custom("Inder", size: 24))
+                    .font(.custom("Inika", size: 36))
+                    .fontWeight(.bold)
                     .foregroundColor(Color(red: 0.65, green: 0.16, blue: 0.16))
+                    .multilineTextAlignment(.leading)
+                    .frame(maxWidth: 300, alignment: .leading)                    
+
+                Text("Your choice helps us find your suitable glasses")
+                    .font(.custom("Inder", size: 20))
+                    .foregroundColor(Color(red: 0.65, green: 0.16, blue: 0.16))
+                    .multilineTextAlignment(.leading)
+                    .frame(maxWidth: .infinity, alignment: .leading)
                 
-                VStack(alignment: .center, spacing: 12) {
-                    
+                VStack(spacing: 24) {
                     Button(action: {
                         print("Female selected")
-                        //
                         isShowingResult.toggle()
                     }) {
                         Text("Female")
-                            .font(Font.custom("Inder", size: 20))
-                            .multilineTextAlignment(.center)
-                            .foregroundColor(Color(red: 0.96, green: 0.96, blue: 0.86))
-                            .frame(width: 203, height: 65)
-                            .background(Color(red: 0.77, green: 0.47, blue: 0.47))
-                            .cornerRadius(12)
+                            .font(.custom("Inder", size: 24))
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, minHeight: 100)
+                            .background(Color(red: 0.85, green: 0.58, blue: 0.58))
+                            .cornerRadius(24)
+                            .shadow(color: .gray.opacity(0.4), radius: 8, x: 0, y: 5)
                     }
 
                     Button(action: {
                         print("Male selected")
-                        //
                         isShowingResult.toggle()
                     }) {
                         Text("Male")
-                            .font(Font.custom("Inder", size: 20))
-                            .multilineTextAlignment(.center)
-                            .foregroundColor(Color(red: 0.96, green: 0.96, blue: 0.86))
-                            .frame(width: 203, height: 65)
-                            .background(Color(red: 0.77, green: 0.47, blue: 0.47))
-                            .cornerRadius(12)
+                            .font(.custom("Inder", size: 24))
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, minHeight: 100)
+                            .background(Color(red: 0.85, green: 0.58, blue: 0.58))
+                            .cornerRadius(24)
+                            .shadow(color: .gray.opacity(0.4), radius: 8, x: 0, y: 5)
                     }
                 }
-                .padding(.bottom, 100)
+                .padding(.bottom, 80)
             }
+            .frame(maxWidth: 350, minHeight: 100)
+            .padding(.bottom, 40)
             
+            
+
+            
+            Spacer()
+
             Text("*Your input shapes your recommendations.")
-                .font(Font.custom("Inder", size: 14))
+                .font(.custom("Inder", size: 14))
                 .foregroundColor(Color(red: 0.65, green: 0.16, blue: 0.16))
+                .padding(.bottom, 30)
         }
         .navigationBarHidden(true)
         .navigationDestination(isPresented: $isShowingResult) {
             CameraView()
         }
     }
-    
 }
 
 #Preview {
--- testingvision/View/WelcomePages.swift
+++ testingvision/View/WelcomePages.swift
@@ -10,37 +10,40 @@ import SwiftUI
 struct WelcomePages: View {
     var body: some View {
         NavigationStack {
-            VStack(alignment: .center, spacing: 0){
-                Image("WelcomeVector")
+            ZStack  {
+                
+                Image("logo")
                     .resizable()
-                    .padding(0)
-                    .scaledToFit()
-                    .scaleEffect(0.7)
+                    .aspectRatio(contentMode: .fill)
+                    .ignoresSafeArea()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
                 
-                Text("WELCOME")
-                    .font(Font.custom("SFProDisplay-Bold", size: 32))
-                    .multilineTextAlignment(.center)
-                    .frame(width: 219, height: 43, alignment: .top)
-                
-                Text("Start scanning your face and let us help you find your suitable glasses")
-                    .font(Font.custom("SFProDisplay-Medium", size: 14))
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(Color(red: 0.65, green: 0.16, blue: 0.16))
-                    .frame(width: 274, alignment: .top)
-                    .padding(.bottom, 32)
-                
-                NavigationLink(destination: GenderView()) {
-                    Text("Find Your Frame")
-                        .font(Font.custom("Inder", size: 16))
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(Color(red: 0.96, green: 0.96, blue: 0.86))
-                        .padding(.horizontal, 24)
-                        .padding(.vertical, 16)
-                        .background(Color(red: 0.77, green: 0.47, blue: 0.47))
-                        .cornerRadius(12)
+                VStack(alignment: .leading, spacing: 0){
+                    
+                    Text("Start scanning your face and let us help you find your suitable glasses")
+                        .font(Font.custom("SFProDisplay-Bold", size: 32))
+                        .multilineTextAlignment(.leading)
+                        .foregroundColor(Color.redMain)
+                        .frame(width: .infinity, height: .infinity, alignment: .top)
+                        .padding(.bottom, 24)
+                    
+                    
+                    NavigationLink(destination: GenderView()) {
+                        Text("Find Your Frame")
+                            .font(Font.custom("Inder", size: 16))
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(Color(red: 0.96, green: 0.96, blue: 0.86))
+                            .padding(.horizontal, 24)
+                            .padding(.vertical, 16)
+                            .background(Color(red: 0.77, green: 0.47, blue: 0.47))
+                            .cornerRadius(12)
+                    }
+                    
+                   
                 }
+                .padding()
             }
-            .padding(.bottom, 100)
+           
         }
 
         
